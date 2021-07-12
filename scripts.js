0bd88dc35b9193ed0412cd56d638b43878370c4c
josephsmith0705 <josephsmith0705@gmail.com>
2021-07-12 14:05:28 +0100

    Various layout changes

--- scripts.js
+++ scripts.js
@@ -1,18 +1,25 @@
-const dropdownButton = document.querySelector('#menu-button');
-const dropdown = document.querySelector('#dropdown-navbar');
-let hidden = true;
-dropdownButton.addEventListener('click', e=>{
-    if (hidden === true){
-        dropdownButton.style.WebkitTransitionDuration='0.5s';
-        dropdownButton.style.WebkitTransform = 'rotate(90deg)';
-        dropdown.style.opacity = '1';
-        dropdown.style.animation = "drop 0.3s ease";
-        hidden = false;
-    } else{
-        dropdownButton.style.WebkitTransitionDuration='0.5s';
-        dropdownButton.style.WebkitTransform = 'rotate(0deg)';   
-        dropdown.style.animation = "fold 0.3s ease";
-        dropdown.style.opacity = '0';
-        hidden = true;     
+const dropdown = {
+    button: document.querySelector('#menu-button'),
+    menu: document.querySelector('#dropdown-navbar'),
+    hidden: true,
+}
+
+dropdown.button.addEventListener('click', e=>{
+    if (dropdown.hidden){
+        dropdown.button.style.transitionDuration = '0.5s';
+        dropdown.button.style.transform = 'rotate(90deg)';
+        dropdown.menu.style.opacity = '1';
+        dropdown.menu.style.animation = 'drop 0.3s ease';
+        dropdown.hidden = false;
+    } else {
+        dropdown.button.style.transform = 'rotate(0deg)';
+        dropdown.menu.style.animation = 'fold 0.3s ease';
+        setTimeout(() => {dropdown.menu.style.opacity = '0';}, 300);
+        dropdown.hidden = true;
     }
 })
+
+const tiles = document.getElementsByClassName('project-tile');
+for (i=0; i<tiles.length; i++){
+    tiles[i].style.transitionDuration = '0.5s';
+}
